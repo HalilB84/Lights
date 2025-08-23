0d1fa35de803262e780cd092966bd2ea08e93787
Halil <hbbattal2006@gmail.com>
2025-08-23 13:15:09 -0400

    Add comments and home screen

--- main.js
+++ main.js
@@ -7,49 +7,68 @@ const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
 const renderer = new THREE.WebGLRenderer();
 document.body.appendChild(renderer.domElement);
 
+const scale = 4;
 const canvas = renderer.domElement;
-const width = window.innerWidth;
-const height = window.innerHeight;
+const width = window.innerWidth / scale;
+const height = window.innerHeight / scale;
 
 const mouse = {x: null, y: null}; 
 
 canvas.addEventListener('mousemove', (e) => {
   const rect = canvas.getBoundingClientRect();
 
-  mouse.x = (e.clientX - rect.left);
-  mouse.y = rect.height - (e.clientY - rect.top);   
-});
+  mouse.x = (e.clientX - rect.left) / scale;
+  mouse.y = (rect.height - (e.clientY - rect.top)) / scale;   
+}); 
 
 const textCanvas = document.createElement('canvas');
 textCanvas.width = width;
 textCanvas.height = height;
 const textCtx = textCanvas.getContext('2d');
 
-let rectX = 0; 
-const rectY = height/1.4;
-const rectWidth = 100;
-const rectHeight = 50;
-const rectSpeed = 2;
+let rects = []
+
+for(let i = 0; i < 15; i++) { // akari style rects
+  let where = Math.random() > 0.5 ? 'left' : 'right';
+  rects.push({
+    where: where,
+    x: where === 'left' ? 0 : width,
+    y: Math.floor(i * width / 14),  // Force integer positions
+    color: `hsl(${Math.random() * 360}, 100%, 50%)`,
+    height: 0.5,
+    width: Math.random() * 100 + 50,
+    speed: Math.random() * 1 + 1,
+  })
+}
 
 
-function drawTextToCanvas(text, x, y, color, fontSize) {
+function drawTextToCanvas(text, x, y, color, fontSize) { 
   textCtx.clearRect(0, 0, textCanvas.width, textCanvas.height);
   
-  textCtx.fillStyle = 'cyan';
-  textCtx.fillRect(rectX, rectY, rectWidth, rectHeight);
-
-  textCtx.fillStyle = 'red';
-  textCtx.fillRect(rectX, rectY - 600, rectWidth, rectHeight);
-
+  rects.forEach(rect => {    
+    textCtx.fillStyle = rect.color;
+    textCtx.fillRect(rect.x, rect.y, rect.width, rect.height);
+  })
 
   
-  rectX += rectSpeed;
-  if (rectX > width ) {
-    rectX = 0 - rectWidth; 
-  }
+  rects.forEach(rect => {
+    if(rect.where === 'left') {
+      rect.x += rect.speed;
+      if (rect.x > width ) {
+        rect.x = 0 - rect.width; 
+        rect.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+      }
+    } else {
+      rect.x -= rect.speed;
+      if (rect.x + rect.width < 0) {
+        rect.x = width; 
+        rect.color = `hsl(${Math.random() * 360}, 100%, 50%)`;
+      }
+    }
+  })
   
-  textCtx.fillStyle = color;
-  textCtx.font = `${fontSize}px Arial`;
+  textCtx.fillStyle = 'orange';
+  textCtx.font = `${24}px Arial`;
   textCtx.textAlign = 'center';
   textCtx.textBaseline = 'middle';
   
@@ -59,7 +78,7 @@ function drawTextToCanvas(text, x, y, color, fontSize) {
 const textTexture = new THREE.CanvasTexture(textCanvas);
 
 
-
+// All render targets are used to render a texture to a texture offscreen
 const rtA = new THREE.WebGLRenderTarget(width, height, {
   minFilter: THREE.NearestFilter,
   magFilter: THREE.NearestFilter,
@@ -73,18 +92,15 @@ const rtB = rtA.clone(); //ping pong element so webgl doesnt yell at me for read
 
 const seedRT = rtA.clone();
 
-// ping pong for jfa
 const jfaA = rtA.clone();
 const jfaB = rtB.clone();
 
 let currentRT = rtA;
 let previousRT = rtB;
 
-
 renderer.setSize(width, height);
 
-
-const geometry = new THREE.PlaneGeometry(2, 2);
+const geometry = new THREE.PlaneGeometry(2, 2); //this is the mesh that will be rendered to the screen. 2,2 because we want it to cover clip space -1 to 1 both x and y
 
 const paintMaterial = new THREE.ShaderMaterial({
   uniforms: {
@@ -92,7 +108,7 @@ const paintMaterial = new THREE.ShaderMaterial({
     mouse: { value: mouse },
     uResolution: { value: new THREE.Vector2(width, height) }
   },
-  vertexShader: `
+  vertexShader: ` //standard vertex shader for the plane, realistically we dont need this but I didnt create a camera for no reason 
     varying vec2 vUv;
     void main() { 
       vUv = uv;
@@ -106,23 +122,18 @@ const paintMaterial = new THREE.ShaderMaterial({
     uniform vec2 mouse;        
     uniform vec2 uResolution;  
 
-    void main() {
+    void main() { 
+        // This is the shader that paints and accumates the color to the texture, used it when I was looking at the tutorial but now its just overriden by the canvas texture, 
+        // I am keeping it here because right now it shows a 10px circle where the mouse is which is fun. 
+
         vec4 color = texture2D(prevTexture, vUv).rgba;
-        
-        if(color.a == 0.0) { //probably not the best way to do this 
-            color = vec4(0.0, 0.0, 0.0, 1.0);
-        }
-
-        vec2 uvPx = vUv * uResolution;
+    
+        vec2 uvPx = vUv * uResolution; //convert stuff the screen space so brush radius can be in pixels
         vec2 mousePx = mouse;
-
-        if(color.x > 0. || color.y > 0. && (color.x != vUv.x || color.y != vUv.y)) { //to be removed
-          //  color = vec4(vUv.x, vUv.y, 0.5, 1.);
-        }
 
         float dist = length(uvPx - mousePx);
 
-        float brushRadius = 10.0; 
+        float brushRadius = 10.0; //brush radius in pixels
         if(dist < brushRadius) {
             color = vec4(vUv.x, vUv.y, 0.5, 1.);
         }
@@ -143,11 +154,15 @@ const seedMaterial = new THREE.ShaderMaterial({
     uniform sampler2D inputTexture;
 
     void main() {
+      //this is the shader that converts the input texture to a seed texture
+      //to calculate the nearest seed in the jfa phase, we need to know the location of the seed in the input texture
+      //so we are storing the uv coordinates in a texture, this creates a nice looking gradient
+      
       vec4 color = texture(inputTexture, vUv);
-      if(color.a == 1.0 && (color.r != 0. || color.g != 0. || color.b != 0.)) {
-        gl_FragColor = vec4(vUv.x, vUv.y, 0.5, 1.);
+      if(color.r != 0. || color.g != 0. || color.b != 0.) { // There is a seed here
+        gl_FragColor = vec4(vUv.x, vUv.y, 0.5, 1.); // vUv.x and vUv.y are the uv coordinates of where this pixel is in, 0.5 is just added to give a nice color scheme
       } else {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+        gl_FragColor = vec4(0.0); //if the color is black, it means there is no seed here thus this will be one of the pixels that shoot rays
       }
     }
   `
@@ -168,10 +183,12 @@ const jfaMaterial = new THREE.ShaderMaterial({
         uniform vec2 resolution;
 
         void main() {
-            vec4 nearestSeed = vec4(0., 0., 0., 1.); //prove why the algo works
-            float nearestDist = 9999999.9;
+            vec4 nearestSeed = vec4(0.0); 
+            float nearestDist = 9.9; // Distance to the nearest seed, we start at 9.9 because that obv larger than any distance in the uv space
 
-            for(float y = -1.; y <= 1.; y += 1.){
+
+            //prove why the algo works
+            for(float y = -1.; y <= 1.; y += 1.){ 
                 for(float x = -1.; x <= 1.; x += 1.){
                     vec2 sampleUV = vUv + vec2(x, y) * offset / resolution; //basically normalizing the target pixel (by offset) in uv coordinates // isnt this prone to rounding errors tho?
                     if (sampleUV.x < 0.0 || sampleUV.x > 1.0 || sampleUV.y < 0.0 || sampleUV.y > 1.0) { continue; }
@@ -205,8 +222,8 @@ const distanceMaterial = new THREE.ShaderMaterial({
 
     void main() { 
 
-      vec2 nearestSeed = texture(inputTexture, vUv).xy;  
-      float dist = clamp(distance(nearestSeed, vUv), 0., 1.);
+      vec2 nearestSeed = texture(inputTexture, vUv).xy;  //YAY we now know where the nearest seed is so just calculate the distance to it
+      float dist = distance(nearestSeed, vUv); //Not sure why we would clamp it tho (in the tutorial)? because 0,0 to 1,1 is ~1.4
       
       gl_FragColor = vec4(vec3(dist), 1.);
     }
@@ -217,7 +234,7 @@ const rayMaterial = new THREE.ShaderMaterial({
   uniforms: {
     iTexture: {value: null},
     distanceTexture: {value: null},
-    rayCount: {value: 64}
+    rayCount: {value: 16}
   },
   vertexShader: paintMaterial.vertexShader,
   fragmentShader: 
@@ -242,31 +259,34 @@ const rayMaterial = new THREE.ShaderMaterial({
 
     vec4 raymarch(){
       vec4 light = texture(iTexture, vUv);
-      if(light.r != 0. || light.g != 0.) {
-        return light;
+      if(light.r != 0. || light.g != 0. || light.b != 0.) { //if we are at a seed location, we dont need to raymarch 
+        return light / 1.5; //lighter color so doesnt mix up with the lighting
       }
 
-      float oneOverRayCount = 1.0 / float(rayCount);
+      float oneOverRayCount = 1.0 / float(rayCount); 
       float tauOverRayCount = TAU * oneOverRayCount;
 
-      float noise = rand(vUv); 
-      vec4 radiance = vec4(0.);
+      float noise = rand(vUv); //noise so rays are offseted from each other
+      vec4 radiance = vec4(0.0); //total light that will be accumulated
 
+    
+      //calcualte and shoot rayCount rays that are equidstant from each other, expensive
       for(int i = 0; i < rayCount; i++) {
         float angle = tauOverRayCount * (float(i) + noise);
         vec2 rayDirection = vec2(cos(angle), -sin(angle));
 
-        vec2 sampleUv = vUv;
+        vec2 sampleUv = vUv; //start at the target pixel
         vec4 radDelta = vec4(0.0);
         
-        for (int step = 1; step < 32; step++) {
+        for (int step = 1; step < 32; step++) { // one funny observation is that pixels that are close to the seed will need more steps to accumulate radiance, this is because since the dist is so small, the rays looking at the other direction (the direction not immediately looking at the seed) will need more steps to reach something else 
           float dist = texture(distanceTexture, sampleUv).r;
           
-          sampleUv += rayDirection * dist;
+          sampleUv += rayDirection * dist; //move the pixel in the direction of the ray, dist is the distance to the nearest seed so we now we can at least move that much
           
           if (outOfBounds(sampleUv)) break;
           
-          if (dist < EPS) {
+          if (dist == 0.0) { //Not sure why we would need EPS? since if we are at a seed location, dist is guaranteed to be 0.0
+            // at this point we now we hit a seed, so get its color and add it to the radiance
             vec4 sampleColor = texture(iTexture, sampleUv);
             radDelta += sampleColor;
             break;
@@ -276,7 +296,7 @@ const rayMaterial = new THREE.ShaderMaterial({
         radiance += radDelta;
       }
 
-      return vec4(max(light, radiance * oneOverRayCount).rgb, 1.0);
+      return vec4((radiance * oneOverRayCount).rgb, 1.0); //finally we return the color of the pixel by averaging the light 
     }
 
     void main() {
@@ -349,16 +369,17 @@ function animate() {
   rayMaterial.uniforms.iTexture.value = currentRT.texture;
   rayMaterial.uniforms.distanceTexture.value = curJFA.texture;
   renderer.setRenderTarget(null); //actual screen
+  renderer.setSize(window.innerWidth, window.innerHeight); //im going to be completely honest, I have no idea why this achieves a crazy amount of speedup, I now akari downsamples and i get the intution but rn this is black magic to me TODO LOG THE SHADERS
 
   renderer.render(scene, camera);
 
   //swap for next frame
-  [currentRT, previousRT] = [previousRT, currentRT];
+  [currentRT, previousRT] = [previousRT, currentRT]; //tbd
 }
 
 renderer.setAnimationLoop(animate);
 
-window.addEventListener('resize', () => {
+window.addEventListener('resize', () => { //resizing is gonna be painful
   const w = window.innerWidth;
   const h = window.innerHeight;
   renderer.setSize(w, h);
@@ -366,5 +387,7 @@ window.addEventListener('resize', () => {
   rtB.setSize(w, h);
   jfaA.setSize(w, h);
   jfaB.setSize(w, h);
+  seedRT.setSize(w, h);
   paintMaterial.uniforms.uResolution.value.set(w, h);
 });
+
